5ebaccb8c58cc6ae6c8dbebb845e31d70601471e
XJMxR <jonathanmosquera2011@gmail.com>
2024-05-13 19:54:42 -0500

    map

--- src/router.js
+++ src/router.js
@@ -3,12 +3,14 @@ import Login from './screens/Login.vue';
 import Register from './screens/Register.vue';
 import Home from './screens/Home.vue';
 import Alquiler_page from "./screens/Alquiler_page.vue";
+import maps_ex from "./screens/maps_ex.vue";
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
   { path: '/login', name: 'Login', component: Login },
   { path: '/signup', name: 'Register', component: Register },
-  { path: '/alquiler', name: 'alquiler', component: Alquiler_page }
+  { path: '/alquiler', name: 'alquiler', component: Alquiler_page },
+  { path: '/mapEx', name: 'map', component: maps_ex }
 ];
 
 const router = createRouter({
---
+++ src/screens/maps_ex.vue
@@ -0,0 +1,48 @@
+<template>
+    <div id="Mainconte">
+        <capacitor-google-map id="map"></capacitor-google-map>
+    </div>
+</template>
+
+
+<style scoped>
+  capacitor-google-map {
+    display: inline-flex;
+    width: 70%;
+    height: 30rem;
+  }
+  .Mainconte{
+    display:flex;
+    align-items: center;
+    align-self: center;
+    align-content: center;
+  }
+  
+</style>
+
+<script setup>
+  import { GoogleMap } from '@capacitor/google-maps';
+import { onMounted } from 'vue';
+
+  const createMap = async () => {
+    const mapRef = document.getElementById('map');
+
+    const newMap = await GoogleMap.create({
+      id: 'my-map', // Unique identifier for this map instance
+      element: mapRef, // reference to the capacitor-google-map element
+      apiKey: 'AIzaSyCc9kjbaLqan7C5zh_l55zpFR2E8lzEQ4w', // Your Google Maps API Key
+      config: {
+        center: {
+          // The initial position to be rendered by the map
+          lat: 3.6,
+          lng: -7.9,
+        },
+        zoom: 8, // The initial zoom level to be rendered by the map
+      },
+    });
+  };
+
+  onMounted(
+    createMap
+  )
+</script>
