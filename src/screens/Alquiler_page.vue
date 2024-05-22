20667efc00d4f726a656c96eae03d06cd9b0d15e
XJMxR <jonathanmosquera2011@gmail.com>
2024-05-22 15:39:52 -0500

    imagenes

--- src/screens/Alquiler_page.vue
+++ src/screens/Alquiler_page.vue
@@ -1,15 +1,19 @@
 <template>
     <body>
+        <header></header>
         <div class="Contenedor">
             <h1></h1>
-            <div class="tarjetas" v-for="vehiculos in v_data":key = "vehiculos"  >
+            <div class="tarjetas" v-for="vehiculos in v_data":key = "vehiculos" >
                 <h1>Categoria: {{vehiculos.Categoria}}</h1>
                 <h3>Marca: {{vehiculos.marca}}</h3>
                 <h3>Modelo: {{vehiculos.modelo}}</h3>
+                <h3>Color:{{ vehiculos.color }}</h3>
                 <h3>Precio de alquiler: {{vehiculos.precio}}</h3>
+                <img :src="vehiculos.imagen">
                 <button @click="$router.push({ path: `/mapEx/${vehiculos.idVehiculos}`,query:{QID:vehiculos.idVehiculos} })">Alquilar</button>
             </div>
         </div>
+        <header></header>
      </body>
 </template>
 <script setup>
@@ -17,7 +21,6 @@ import axios from 'axios';
 import { onBeforeMount, ref } from 'vue';
 
 const v_data = ref("");
-console.log(v_data)
 
 
 async function cargar_vehiculos() {
@@ -57,7 +60,7 @@ cargar_vehiculos
 )
 
 </script>
-<style >
+<style scoped >
 
 .tarjetas {
     color: aliceblue;
@@ -85,8 +88,25 @@ cargar_vehiculos
 button{
     color: black;
     background-color: aliceblue;
-    border-radius: 10%;
     width: 70%;
 }
-
+img{
+    border-radius: 1rem;
+    gap:1rem;
+    -webkit-transform: scale(1,1);
+    -webkit-transition-timing-function: ease-out;
+    -webkit-transition-duration: 550ms;
+    -moz-transform: scale(1,1);
+    -moz-transition-timing-function: ease-out;
+    -moz-transition-duration: 500ms;
+}
+img:hover{
+    border-radius: 0%;
+    -webkit-transform: scale(1.12,1.12);
+    -webkit-transition-timing-function: ease-out;
+    -webkit-transition-duration: 500ms;
+    -moz-transform: scale(1.5,1.5);
+    -moz-transition-timing-function: ease-out;
+    -moz-transition-duration: 500ms;
+}
 </style>
